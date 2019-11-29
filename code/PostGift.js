4d094b4a80981a361af102f4b557658c7788b505
young31 <migael38317@gmail.com>
2019-11-30 05:45:31 +0900

    targetphone

--- code/PostGift.js
+++ code/PostGift.js
@@ -1,10 +1,15 @@
-module.exports.function = function postGift (name, url) {
+module.exports.function = function postGift(name, url, targetphone) {
   const http = require('http')
-  const target_url = ''
-  let meassge = 'https://message-234234234-server.herokuapp.com/api/v1/send/'
-  const res = http.getUrl(target_url)
+  const target_url = 'https://message-234234234-server.herokuapp.com/api/v1/send/'
+  let meassge = ''
+  const params = {
+    name: name,
+    url: url,
+    targetphone: targetphone
+  }
+  const res = http.postUrl(target_url, params, { format: 'json' })
 
-  if(res) {
+  if (res) {
     message = '성공적으로 선물을 보냈습니다.'
   } else {
     message = '선물보내기가 실패하였습니다.'
