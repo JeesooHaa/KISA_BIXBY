9a71b6cc6fa9400f040e2e7045ca43e63ba0c673
JeesooHa <haajeesoo@gmail.com>
2019-11-30 01:41:40 +0900

    Add insure view

--- code/Insure.js
+++ code/Insure.js
@@ -1,18 +1,20 @@
-module.exports.function = function insure (caetgory, insureinput, goalinput) {
+module.exports.function = function insure(caetgory, insureinput, goalinput) {
   const db = require('./db.js').data // 카테고리분기 준비
   const console = require('console')
 
   let res = []
-  for(i in db) {
+  for (let i in db) {
     let rate_min = []
     const rm = Object.keys(db[i]['최저이율'])
     let idx = 0
-    for(j in db[i]['최저이율']) {
+    // console.log(typeof i)
+    for (j in db[i]['최저이율']) {
+      console.log(Number(i))
       rate_min.push({
-        name: rm[idx],
+        name: rm[Number(i)],
         rate: db[i]['최저이율'][j]
       })
-      idx++
+      idx += 1
     }
     const insureset = {
       rate: db[i]["공시이율"],
