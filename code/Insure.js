b159d8b78b546aa682d399b49ebe9837e6752cd1
young31 <migael38317@gmail.com>
2019-11-30 01:21:24 +0900

    insure

--- code/Insure.js
+++ code/Insure.js
@@ -1,32 +1,31 @@
-module.exports.function = function insure (caetgory, insureinput, goalinput) {
+module.exports.function = function insure(caetgory, insureinput, goalinput) {
   const db = require('./db.js').data // 카테고리분기 준비
   const console = require('console')
 
   let res = []
-  for(i in db) {
-    let rate_min = []
-    const rm = Object.keys(db[i]['최저이율'])
-    let idx = 0
-    for(j in db[i]['최저이율']) {
-      rate_min.push({
-        name: rm[idx],
-        rate: db[i]['최저이율'][j]
-      })
-      idx++
-    }
-    const insureset = {
+  // console.log(Object.keys(db))
+  for (let i in db) {
+    var temp = {
+      name: db[i].name,
       rate: db[i]["공시이율"],
-      rate_min: rate_min,
+      rate_min: [],
       TR: db[i]['해지환급금'],
       TR_rate: db[i]['환급률']
     }
-
-    res.push(insureset)
+    let kk = Object.keys(db[i]['최저이율'])
+    let idx = 0
+    for (j in db[i]['최저이율']) {
+      temp.rate_min.push({
+        name: kk[idx],
+        rate: db[i]['최저이율'][j],
+      })
+      idx++
+    }
+    res.push(temp)
   }
-
-  console.log(res)
 
   return {
     insureset: res
   }
+
 }
